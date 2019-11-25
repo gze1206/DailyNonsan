e6e897dbc79707f7c67647d4e19e0261745a830b
gze1206 <gze1206@naver.com>
2019-11-26 02:45:54 +0900

    init ORM

---
+++ src/entity/User.ts
@@ -0,0 +1,18 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number = 0;
+
+    @Column()
+    firstName: string = '';
+
+    @Column()
+    lastName: string = '';
+
+    @Column()
+    age: number = -1;
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { User } from "./entity/User";
+
 import { WebServer } from './web';
 
 (async () => {
@@ -10,4 +14,21 @@ import { WebServer } from './web';
     });
     web.run(5252);
 
+    createConnection().then(async connection => {
+
+        console.log("Inserting a new user into the database...");
+        const user = new User();
+        user.firstName = "Timber";
+        user.lastName = "Saw";
+        user.age = 25;
+        await connection.manager.save(user);
+        console.log("Saved a new user with id: " + user.id);
+
+        console.log("Loading users from the database...");
+        const users = await connection.manager.find(User);
+        console.log("Loaded users: ", users);
+
+        console.log("Here you can setup and run express/koa/any other framework.");
+
+    }).catch(error => console.log(error));
 })();
