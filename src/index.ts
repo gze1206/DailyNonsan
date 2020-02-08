e165f6741c76a8fb13d6fbcc16f30b351632ca28
gze1206 <gze1206@naver.com>
2020-02-08 23:17:18 +0900

    Add crontab

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
 import "reflect-metadata";
+import { CronJob } from "cron";
 import { createConnection } from "typeorm";
 
 import { WebServer } from './web';
 import { SettingManager } from "./settingManager";
-import { readFileSync } from "fs";
-import NewsModule from "./modules/news";
-import ModuleBase from "./modules/moduleBase";
-import MelonModule from "./modules/melon";
-import ExchangeModule from "./modules/exchange";
-import LetterManager from "./letterManager";
 import PreviewRoute from "./routes/api/preview";
 import SendRoute from "./routes/api/send";
 import PageRoute from "./routes/pages";
+import Axios from "axios";
+
+const port: number = 5252;
 
 (async () => {
 
@@ -21,11 +19,19 @@ import PageRoute from "./routes/pages";
 
         SettingManager.SetupRoute(web, connection);
 
+        SetupCron();
+        web.use('/cron', function (req, res, next) {
+            if (JOB_sendAll) {
+                JOB_sendAll.fireOnTick();
+            }
+            res.status(200).send('DONE');
+        });
+
         PreviewRoute(web, connection);
         SendRoute(web, connection);
         PageRoute(web, connection);
 
-        web.run(5252);
+        web.run(port);
         console.log(await SettingManager.GetOption(connection, SettingManager.KEY_TRAINEE_NAME, true));
         console.log(await SettingManager.GetOption(connection, SettingManager.KEY_UNIT_NAME, true));
 
@@ -34,3 +40,22 @@ import PageRoute from "./routes/pages";
 
     }).catch(error => console.log(error));
 })();
+
+let JOB_sendAll: CronJob | null = null;
+function SetupCron() {
+    JOB_sendAll = new CronJob('0 5 * * *', async () => {
+        const axios = Axios.create({
+            baseURL: `http://127.0.0.1:${port}/api/send`,
+            timeout: 1000,
+        });
+        if (axios == null) {
+            console.error('[CRON] CLIENT INIT FAILED!');
+            return;
+        }
+
+        await axios.post('all');
+        console.log(`${new Date().toISOString()} - Cron worked!`);
+    });
+
+    JOB_sendAll.start();
+}
